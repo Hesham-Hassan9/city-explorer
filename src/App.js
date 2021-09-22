a6d7f49cf5d946e92c086b87f48bf6ef6613cad8
Hesham-Hassan9 <19012748@student.ltuc.com>
2021-09-22 19:32:27 +0300

    solving lab09

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-import React from 'react';
-import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Image from 'react-bootstrap/Image'
-import Weather from './Components/Weather.js'
-import Movies from './Components/Movies.js'
-import './Style.css';
-
+import React from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
+import Weather from "./Components/Weather.js";
+import Movies from "./Components/Movies.js";
+import "./Style.css";
 
 class App extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
       result: {},
       forecastArr: [],
       forecastArrMovies: [],
-      search: '',
+      search: "",
       showInfo: false,
       showError: false,
-    }
+    };
   }
 
   getLocFun = async (e) => {
     e.preventDefault();
     await this.setState({
-      search: e.target.city.value
-
-    })
+      search: e.target.city.value,
+    });
     try {
       let reqUrl = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.search}&format=json`;
 
@@ -36,28 +33,29 @@ class App extends React.Component {
       this.setState({
         result: locResult.data[0],
         showInfo: true,
-        showError: false
-      })
+        showError: false,
+      });
     } catch {
-      console.log('something went wrong')
+      console.log("something went wrong");
       this.setState({
         showError: true,
-        showInfo: false
-      })
+        showInfo: false,
+      });
     }
     this.getWeather();
     this.getMovies();
-  }
+  };
 
   getWeather = async () => {
     const WEATHER = `${process.env.REACT_APP_LOCATIONIQ_LINK}/getweather?searchQuery=${this.state.search}`;
     const localhost = `http://localhost:3002/getweather?searchQuery=${this.state.search}`;
+    // const WEATHER = `https://city-explorer-api9.herokuapp.com/getweather?searchQuery=Amman`;
 
     console.log(localhost);
     const weatherResponse = await axios.get(WEATHER);
-    console.log(weatherResponse.data);
+    // console.log(weatherResponse.data);
     await this.setState({ forecastArr: weatherResponse.data });
-  }
+  };
 
   getMovies = async () => {
     const MoviesURL = `${process.env.REACT_APP_LOCATIONIQ_LINK}/getmovies?searchQuery=${this.state.search}`;
@@ -65,54 +63,52 @@ class App extends React.Component {
 
     console.log(localhost);
     const moviesResponse = await axios.get(MoviesURL);
-    console.log(moviesResponse.data);
+    // console.log(moviesResponse.data);
     await this.setState({ forecastArrMovies: moviesResponse.data });
-  }
-
+  };
 
   render() {
     return (
       <>
         <Form onSubmit={this.getLocFun}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label >City Name</Form.Label>
-            <Form.Control type="text" name="city" placeholder="Enter City Name" />
+            <Form.Label>City Name</Form.Label>
+            <Form.Control
+              type="text"
+              name="city"
+              placeholder="Enter City Name"
+            />
           </Form.Group>
           <Button variant="primary" type="submit">
             Explore!
           </Button>
         </Form>
         <br />
-        {
-          this.state.showInfo &&
+        {this.state.showInfo && (
           <>
             <p>City name: {this.state.search}</p>
             <p>latitude: {this.state.result.lat}</p>
             <p>longitude: {this.state.result.lon} </p>
-            <Image src={`https://maps.locationiq.com/v3/staticmap?key=f5de8e48adbdc6&center=${this.state.result.lat},${this.state.result.lon}&zoom=10`} alt="city" width={800} height={825} id="map" />
-
-            {this.state.forecastArr.map(info => {
-              return (
-                <Weather forecastArr={info} />
-                
-              )
+            <Image
+              src={`https://maps.locationiq.com/v3/staticmap?key=f5de8e48adbdc6&center=${this.state.result.lat},${this.state.result.lon}&zoom=10`}
+              alt="city"
+              width={800}
+              height={825}
+              id="map"
+            />
+            {this.state.forecastArr.map((info) => {
+              return <Weather forecastArr={info} />;
             })}
-
-{this.state.forecastArrMovies.map(info => {
-              return (
-                <Movies forecastArrMovies={info}/>
-              )
+            {this.state.forecastArrMovies.map((info) => {
+              return <Movies forecastArrMovies={info} />;
             })}
-
           </>
-        }
-        {this.state.showError &&
+        )}
+        {this.state.showError && (
           <p>Something went wrong in getting location data</p>
-        }
-
-
+        )}
       </>
-    )
+    );
   }
 }
 
